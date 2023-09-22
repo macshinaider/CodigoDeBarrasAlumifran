06042a3c25bfda13c08755ca6112ec0d23a38117
macshinaider <lucasadd15@gmail.com>
2023-09-22 15:39:49 -0300

    alterando

--- backend/prisma/migrations/20230922145625_second/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[procod]` on the table `AlumifranPrecos` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "AlumifranPrecos_procod_key" ON "AlumifranPrecos"("procod");
