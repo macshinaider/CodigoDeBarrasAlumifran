8287c5e41608b1bcd798ece6c1b2da540be18b93
macshinaider <lucasadd15@gmail.com>
2023-10-21 10:54:29 -0300

    alterando

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,4 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -8,5 +7,12 @@ export default {
     extend: {},
   },
   plugins: [],
+  variants: {},
+  corePlugins: {},
+  // Adicione a classe de animação personalizada
+  extend: {
+    animation: {
+      'marquee': 'marquee 5s linear infinite',
+    },
+  },
 }
-
