48fe22470c7032f7b71514bfeef6140d7894653c
Lucas Santana <lucasadd15@gmail.com>
2024-01-05 15:35:51 -0300

    alter code

--- backend/src/function/rabbitmq/config.ts
+++ backend/src/function/rabbitmq/config.ts
@@ -1,5 +1,5 @@
 export const rabbitMQConfig = {
-  hostname: '89.117.51.204', // Hostname do RabbitMQ (mude para o seu ambiente)
+  hostname: '161.97.86.2', // Hostname do RabbitMQ (mude para o seu ambiente)
   port: 5672,            // Porta padrão do RabbitMQ
   username: 'luuhsantanafs',     // Nome de usuário (padrão: guest)
   password: 'Lucas102030',
