35b5e92c46648b4ac0eb518df2aa89bc3cd9c7ad
Lucas Santana <lucasadd15@gmail.com>
2024-03-07 22:36:26 -0300

    Update config.ts

--- backend/src/function/rabbitmq/config.ts
+++ backend/src/function/rabbitmq/config.ts
@@ -1,5 +1,5 @@
 export const rabbitMQConfig = {
-  hostname: 'jackal-01.rmq.cloudamqp.com', // Hostname do RabbitMQ (mude para o seu ambiente)
+  hostname: 'jackal.rmq.cloudamqp.com', // Hostname do RabbitMQ (mude para o seu ambiente)
   port: 5672,            // Porta padrão do RabbitMQ
   username: 'bajxpiuy',     // Nome de usuário (padrão: guest)
   password: '9ny3WL19iChk9DUEIm99DO5x3UOI6tXR',
