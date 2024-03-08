9b3cd38b15f7592961c20f55bfb4c51c7243494d
Lucas Santana <lucasadd15@gmail.com>
2024-03-07 22:43:00 -0300

    Update config.ts

--- backend/src/function/rabbitmq/config.ts
+++ backend/src/function/rabbitmq/config.ts
@@ -1,5 +1,5 @@
 export const rabbitMQConfig = {
-  hostname: 'apirabbitmq.alumifrandescartaveis.com.br', // Hostname do RabbitMQ (mude para o seu ambiente)
+  hostname: 'consultapreco_rabbitmq', // Hostname do RabbitMQ (mude para o seu ambiente)
   port: 5672,            // Porta padrão do RabbitMQ
   username: 'luuhsantanafs',     // Nome de usuário (padrão: guest)
   password: 'Lucas102030',
